783deb6cd66fcd603b5a74a91dac2e065d61d9eb
munhyunsu <munhyunsu@gmail.com>
2018-05-30 18:36:47 +0900

    utils

---
+++ utils.py
@@ -0,0 +1,15 @@
+def should_post(salmon1_start, salmon1_end, now = datetime.datetime.now()):
+    start = False
+    plan = False
+    end = False
+    start_sec = (salmon1_start - now).total_seconds()
+    if (start_sec <= (3600-1800)) and (start_sec > (0-1800)):
+        start = True
+    if (start_sec <= (21600+1800)) and (start_sec > (18000+1800)):
+        plan = True
+    end_sec = (salmon1_end - now).total_seconds()
+    if (end_sec <= (0+1800)) and (end_sec > (-3600+1800)):
+        end = True
+
+    return (start, plan, end)
+
