bf0d1d8e45cdfac69aa3b735a41ca4064a1790b9
Hyun-Su, Mun <munhyunsu@gmail.com>
2018-06-06 17:22:28 +0900

    remove prints

--- twitter.py
+++ twitter.py
@@ -8,22 +8,18 @@ def get_api():
     api = tweepy.API(auth)
 
     return api
-# user = api.me()
-# print('User name: {0}'.format(user.name))
+
 
 
 def execute_retweet():
     api = get_api()
     # Search tweets contain keywords and retweet them
-    # Need to run every hour
     try:
         for tweet in tweepy.Cursor(api.search, q='#연어런').items(10):
-            # Add \n escape character to print() to organize tweets
-            # print('\nTweet by: @' + tweet.user.screen_name)
             tweet.retweet()
-            # print('Retweeted the tweet')
     except:
         pass
+
 
 
 def post_tweet(text):
