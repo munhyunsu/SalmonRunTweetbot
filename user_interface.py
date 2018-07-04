a535e9d3f0149dc510700ccc759dc5efd901169f
Hyun-Su, Mun <munhyunsu@gmail.com>
2018-07-04 16:55:56 +0900

    interactive program

---
+++ user_interface.py
@@ -0,0 +1,13 @@
+import sys
+
+status = {'INIT': 0}
+
+class UserInterface(object):
+    def __init__(self):
+        self.status = status['INIT']
+
+    def first_screen(self):
+        pass
+
+    def schedule(self):
+        pass
