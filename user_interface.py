e9b44d49a95d6f04ce5b162020ff0739c59f6c46
Hyun-Su, Mun <munhyunsu@gmail.com>
2018-07-06 10:46:02 +0900

    Viewer

--- user_interface.py
+++ user_interface.py
@@ -22,8 +22,53 @@ class UserInterface(object):
         (salmon1_stage, salmon2_stage) = crawler.get_stage()
         (start, plan, end) = should_post(salmon1_times)
 
+        start_text = '''[연어런 시작]
+시간: {0} - {1}
+스테이지: {2}/{3}
+무기: {4}/{5}
+{6}/{7}
+{8}/{9}
+{10}/{11}'''.format(salmon1_times[0].strftime('%m/%d %H:%M'), salmon1_times[1].strftime('%m/%d %H:%M'),
+                    salmon1_stage, st_en_jp[salmon1_stage],
+                    salmon1_weapons[0], wp_en_jp[salmon1_weapons[0]],
+                    salmon1_weapons[1], wp_en_jp[salmon1_weapons[1]],
+                    salmon1_weapons[2], wp_en_jp[salmon1_weapons[2]],
+                    salmon1_weapons[3], wp_en_jp[salmon1_weapons[3]])
+
+        plan_text = '''[연어런 예정]
+시간: {0} - {1}
+스테이지: {2}/{3}
+무기: {4}/{5}
+{6}/{7}
+{8}/{9}
+{10}/{11}'''.format(salmon1_times[0].strftime('%m/%d %H:%M'), salmon1_times[1].strftime('%m/%d %H:%M'),
+                    salmon1_stage, st_en_jp[salmon1_stage],
+                    salmon1_weapons[0], wp_en_jp[salmon1_weapons[0]],
+                    salmon1_weapons[1], wp_en_jp[salmon1_weapons[1]],
+                    salmon1_weapons[2], wp_en_jp[salmon1_weapons[2]],
+                    salmon1_weapons[3], wp_en_jp[salmon1_weapons[3]])
+
+        end_text = '''[연어런 끝/다음 연어런]
+        시간: {0} - {1}
+        스테이지: {2}/{3}
+        무기: {4}/{5}
+        {6}/{7}
+        {8}/{9}
+        {10}/{11}'''.format(salmon2_times[0].strftime('%m/%d %H:%M'), salmon2_times[1].strftime('%m/%d %H:%M'),
+                            salmon2_stage, st_en_jp[salmon2_stage],
+                            salmon2_weapons[0], wp_en_jp[salmon2_weapons[0]],
+                            salmon2_weapons[1], wp_en_jp[salmon2_weapons[1]],
+                            salmon2_weapons[2], wp_en_jp[salmon2_weapons[2]],
+                            salmon2_weapons[3], wp_en_jp[salmon2_weapons[3]])
+
         #TODO(LuHa): beautify
         print('----*----*----*----*----*----*')
-        print(datetime.datetime.now(), salmon1_times, start, plan, end)
+        print('Start {0}'.format(start))
+        print(start_text)
+        print('----*----*----*')
+        print('Plan {0}'.format(plan))
+        print(plan_text)
+        print('----*----*----*')
+        print('End {0}'.format(end))
+        print(end_text)
         print('----*----*----*----*----*----*')
-
