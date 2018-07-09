695829951c0abe08180d82037958061c25be5b7c
Hyun-Su, Mun <munhyunsu@gmail.com>
2018-07-09 18:57:50 +0900

    Scheduler

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,45 @@
-import sys
+import urllib
+import pickle
 
 class Scheduler(object):
-    def __init__(self):
+    def __init__(self, baseurl = 'https://splatoonwiki.org'):
+        self.baseurl = baseurl
+        self.mainpage = self._get_mainpage()
+        self.data = self._get_update_pickle()
+
+
+    def _get_mainpage(self):
+        url = self.baseurl + '/wiki/Main_Page'
+        with urllib.request.urlopen(url) as f:
+            mainpage = f.read()
+        return mainpage
+
+
+    def _get_update_pickle(self):
+        '''
+        pickle data form
+        [{'start_time': ,
+          'end_time': ,
+          'weapon1': ,
+          'weapon2': ,
+          'weapon3': ,
+          'weapon4': ,
+          'stage': ,},
+          {...}, {...}]
+        :return:
+        '''
+        # with open('salmon.pickle','rb') as p:
+
+        pass
+
+
+    def get_schedule(self):
+        pass
+
+
+    def get_weapon(self):
+        pass
+
+
+    def get_stage(self):
         pass
