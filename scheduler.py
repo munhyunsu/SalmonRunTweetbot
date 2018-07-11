ab70b4ce58db944e3320701d4b85a73050ed4de7
Hyun-Su, Mun <munhyunsu@gmail.com>
2018-07-11 14:52:17 +0900

    append schedule

--- scheduler.py
+++ scheduler.py
@@ -28,6 +28,15 @@ class Scheduler(WebCrawler):
         (salmon1_times, salmon2_times) = self.get_schedule()
         (salmon1_weapons, salmon2_weapons) = self.get_weapon()
         (salmon1_stage, salmon2_stage) = self.get_stage()
+        schedule = {'start_time': salmon1_times[0],
+                    'end_time': salmon1_times[1],
+                    'weapon1': salmon1_weapons[0],
+                    'weapon2': salmon1_weapons[1],
+                    'weapon3': salmon1_weapons[2],
+                    'weapon4': salmon1_weapons[3],
+                    'stage': salmon1_stage}
+        schedule_list = list()
+        schedule_list.append(schedule)
         # with open('salmon.pickle','rb') as p:
 
         pass
