53732336390c77231596e387b7438141e8c0ebb3
Hyun-Su, Mun <munhyunsu@gmail.com>
2018-07-26 08:50:57 +0900

    new test for scheduler

---
+++ tests/scheduler_tests.py
@@ -0,0 +1,11 @@
+import unittest
+
+import scheduler
+
+class SchedulerTests(unittest.TestCase):
+    def setUp(self):
+        self.scheduler = scheduler.Scheduler
+        pass
+
+    def tearDown(self):
+        pass
