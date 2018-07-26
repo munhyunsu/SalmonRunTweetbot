18bc806bde7208a964df7179305c90ecaedb1562
Hyun-Su, Mun <munhyunsu@gmail.com>
2018-07-26 18:51:46 +0900

    scheduler test functions

--- tests/scheduler_tests.py
+++ tests/scheduler_tests.py
@@ -2,10 +2,20 @@ import unittest
 
 import scheduler
 
+
 class SchedulerTests(unittest.TestCase):
     def setUp(self):
-        self.scheduler = scheduler.Scheduler
+        self.scheduler = scheduler.Scheduler()
         pass
 
     def tearDown(self):
+        pass
+
+    def test_get_mainpage(self):
+        pass
+
+    def test_get_update_pickle(self):
+        pass
+
+    def test_get_schedule_list(self):
         pass
