3af43815ff861d2371e8ed3fda03ff5d095d9145
Hyun-Su, Mun <munhyunsu@gmail.com>
2018-08-15 14:49:13 +0900

    new tests

--- tests/image_handler_tests.py
+++ tests/image_handler_tests.py
@@ -1,1 +1,13 @@
+import unittest
+
 from PIL import image
+
+
+class ImageHandlerTests(unittest.TestCase):
+    """How test this module?
+    """
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
