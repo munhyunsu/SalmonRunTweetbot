63122da82f2b0cded45947ad9e2a3dec3e510ae6
Hyun-Su, Mun <munhyunsu@gmail.com>
2018-10-20 18:25:37 +0900

    text style

--- tests/inkipedia_provider_tests.py
+++ tests/inkipedia_provider_tests.py
@@ -15,27 +15,27 @@ class InkipediaProviderTestCase(unittest.TestCase):
 
     def test_get_regular(self):
         ctx_mock = unittest.mock.MagicMock()
-        self.assertEqual(('[Regular Battle]\n'
-                          'Now, Turf War\n'
+        self.assertEqual(('**[Regular Battle]**\n'
+                          'Now, __Turf War__\n'
                           'Starfish Mainstage, The Reef\n'
-                          'Next, Turf War\n'
+                          'Next, __Turf War__\n'
                           'Sturgeon Shipyard, Camp Triggerfish\n'),
                          self.reader.get_regular(ctx_mock))
 
     def test_get_ranked(self):
         ctx_mock = unittest.mock.MagicMock()
-        self.assertEqual(('[Ranked Battle]\n'
-                          'Now, Clam Blitz\n'
+        self.assertEqual(('**[Ranked Battle]**\n'
+                          'Now, __Clam Blitz__\n'
                           'Manta Maria, Moray Towers\n'
-                          'Next, Tower Control\n'
+                          'Next, __Tower Control__\n'
                           'Snapper Canal, Port Mackerel\n'),
                          self.reader.get_ranked(ctx_mock))
 
     def test_get_league(self):
         ctx_mock = unittest.mock.MagicMock()
-        self.assertEqual(('[League Battle]\n'
-                          'Now, Tower Control\n'
+        self.assertEqual(('**[League Battle]**\n'
+                          'Now, __Tower Control__\n'
                           'Wahoo World, MakoMart\n'
-                          'Next, Rainmaker\n'
+                          'Next, __Rainmaker__\n'
                           'Moray Towers, Musselforge Fitness\n'),
                          self.reader.get_league(ctx_mock))
