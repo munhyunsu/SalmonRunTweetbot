6defd1e59df87d743b9b1bd6c5fdd4db98adf1b4
munhyunsu <munhyunsu@gmail.com>
2019-02-25 10:00:49 +0900

    new way to working

--- discord_bot/modules/meme_loader.py
+++ discord_bot/modules/meme_loader.py
@@ -1,3 +1,6 @@
+import random
+
+
 class MemeLoader(object):
     def __init__(self, memes):
         self.memes = memes
@@ -6,9 +9,11 @@ class MemeLoader(object):
         if len(args) < 1:
             return '현재 짤이 {0}개 있습니다.'.format(len(self.memes))
         else:
-            called_meme = args[0]
-            called_meme_url = self.memes.get(called_meme, None)
-            if called_meme_url is None:
-                return '검색된 짤이 없습니다.'
-            else:
-                return called_meme_url
+            # Change a way to working
+            # called_meme = args[0]
+            # called_meme_url = self.memes.get(called_meme, None)
+            # if called_meme_url is None:
+            #     return '검색된 짤이 없습니다.'
+            # else:
+            #     return called_meme_url
+            return random.choice(list(self.memes.values()))
--- discord_bot/salmonrun_commands.py
+++ discord_bot/salmonrun_commands.py
@@ -9,7 +9,7 @@ from .modules.inkipedia_provider import InkipediaProvider
 FILENAME = 'latest_url'
 
 
-class SalmonrunCommands(object):
+class SplatoonCommands(object):
     def __init__(self):
         gspread = GSpreadHandler('private/Gspread-39d43309c65c.json')
         (wp_en_jp, wp_en_ko, st_en_jp, st_en_ko) = gspread.get_translate_dict()
@@ -25,39 +25,39 @@ class SalmonrunCommands(object):
     @commands.command(name='salmonrun',
                       aliases=['연어런', '연어'])
     async def salmonrun(self, ctx, *args):
-        """SalmonRunKR의 최신 트윗을 가져옵니다."""
+        """연어런 알림 봇의 최신 트윗을 가져옵니다. '!연어'"""
         await ctx.send(self.file_reader.get_file_content(ctx, args))
 
     @commands.command(name='meme',
                       aliases=['짤'])
     async def meme(self, ctx, *args):
-        """짤을 호출합니다(URL)."""
+        """짤방을 무작위로 호출합니다. '!짤'"""
         await ctx.send(self.meme_loader.get_meme(ctx, args))
 
     @commands.command(name='weapon',
                       aliases=['무기'])
     async def weapon(self, ctx, *args):
-        """무작위 무기를 호출합니다."""
+        """무작위 무기를 호출합니다. '!무기'"""
         await ctx.send(self.random_selector.get_random(ctx, args))
 
     @commands.command(name='regular',
                       aliases=['일반', '나와바리'])
     async def regular(self, ctx, *args):
-        """일반 배틀 스테이지를 확인합니다."""
+        """레귤러/나와바리 배틀 스테이지를 확인합니다. '![나와바리or일반]'"""
         await ctx.send(self.inkipedia.get_regular(ctx, args))
 
     @commands.command(name='ranked',
                       aliases=['랭크'])
     async def ranked(self, ctx, *args):
-        """랭크 배틀 스테이지를 확인합니다."""
+        """랭크 배틀 스테이지를 확인합니다. '!랭크'"""
         await ctx.send(self.inkipedia.get_ranked(ctx, args))
 
     @commands.command(name='league',
                       aliases=['리그'])
     async def league(self, ctx, *args):
-        """리그 배틀 스테이지를 확인합니다."""
+        """리그 배틀 스테이지를 확인합니다. '!리그'"""
         await ctx.send(self.inkipedia.get_league(ctx, args))
 
 
 def setup(bot):
-    bot.add_cog(SalmonrunCommands())
+    bot.add_cog(SplatoonCommands())
