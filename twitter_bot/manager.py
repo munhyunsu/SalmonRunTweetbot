76610fd44396f7d29e730d0cdfb1ef7165c39869
munhyunsu <munhyunsu@gmail.com>
2019-04-26 18:29:39 -0700

    unused function

--- twitter_bot/manager.py
+++ twitter_bot/manager.py
@@ -3,25 +3,6 @@ import re
 import importlib
 import pkgutil
 
-def get_files(path, ext='', recursive=False):
-    """
-    Read all files in path
-    :param path: path for reading
-    :return: absolute path of all files in directory list
-    """
-    path_list = [path]
-
-    while len(path_list) > 0:
-        cpath = path_list.pop()
-        with os.scandir(cpath) as it:
-            for entry in it:
-                if not entry.name.startswith('.') and entry.is_file():
-                    if entry.name.endswith(ext):
-                        yield entry.path
-                    else:
-                        if recursive:
-                            path_list.append(entry.path)
-
 
 def main():
     collectors = list()
