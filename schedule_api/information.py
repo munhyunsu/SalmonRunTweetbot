d585601d618b141ca13832b741827513f9d4eff3
munhyunsu <munhyunsu@gmail.com>
2022-08-25 20:42:00 +0900

    API description

---
+++ schedule_api/information.py
@@ -0,0 +1,10 @@
+title = 'Splatoon 3 Schedule API'
+description = '''Unofficial Splatoon 3 schedule API
+'''
+version = 0.1.0
+contact = {
+  'name': 'SalmonRunKR',
+  'url': 'https://twitter.com/SalmonRunKR',
+}
+license_info = {
+}
