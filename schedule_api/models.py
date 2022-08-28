76ee67448522d53bf2431ebf0f303333388ef449
munhyunsu <munhyunsu@gmail.com>
2022-08-28 10:05:18 +0900

    model

---
+++ schedule_api/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class Salmonrun(Base):
+    __tablename__ = 'salmonrun'
+
+    timestart = Column(Integer, primary_key=True, index=True)
+    timeend = Column(Integer, unique=True, index=True)
+    stage = Column(String)
+    weapon1 = Column(String)
+    weapon2 = Column(String)
+    weapon3 = Column(String)
+    weapon4 = Column(String)
+
