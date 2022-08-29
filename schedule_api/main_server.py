5d13b229d67b058f9ce66490ca1c90dcf2ff13c9
munhyunsu <munhyunsu@gmail.com>
2022-08-30 06:55:17 +0900

    main api server

--- schedule_api/main_server.py
+++ schedule_api/main_server.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
+import time
 
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
 import config
 import information
 
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
   title=information.title,
@@ -12,13 +19,37 @@ app = FastAPI(
   license_info=information.license_info,
   root_path=config.root_path)
 
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.on_event('startup')
-async def startup_event():
+def startup_event():
     pass
 
 
 @app.on_event('shutdown')
-async def shutdown_event():
+def shutdown_event():
     pass
+
+
+@app.post('/salmonrun/', response_model=schemas.Salmonrun)
+def create_salmonrun(salmonrun: schemas.SalmonrunCreate, db: Session = Depends(get_db)):
+    db_salmonrun = crud.get_salmonrun_by_timestart(db, timestart=salmonrun.timestart)
+    if db_salmonrun:
+        raise HTTPException(status_code=404, detail='timestart already registered')
+    return crud.create_salmonrun(db=db, salmonrun=salmonrun)
+
+
+@app.get('/salmonrun/', response_model=list[schemas.Salmonrun])
+def read_salmonruns(timequery: Union[int, None] = None, db: Session = Depends(get_db)):
+    if timequery is None:
+        timequery = int(time.time())
+    salmonruns = crud.get_salmonruns(db, timequery=timequery)
+    return salmonruns
 
