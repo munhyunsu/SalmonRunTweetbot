cd257d73583e6cd32ee730fea141e93bc0fe2a29
munhyunsu <munhyunsu@gmail.com>
2022-08-29 19:39:47 +0900

    config template

--- schedule_api/config.py
+++ schedule_api/config.py
@@ -1,4 +1,6 @@
 address_api = '127.0.0.1' # Only localhost for serving
 port_api = 10008 # FastAPI serving port
+root_path = 'splatoon3/' # API endpoint for serving
 freshness = 60*60 # int (seconds)
+db_url = './app.db' # SQLite3 database for API app
 
