1065efee37c115f75311a6c3b0b4ab434f9c73fb
munhyunsu <munhyunsu@gmail.com>
2022-09-01 19:28:30 +0900

    typo version

--- schedule_api/information.py
+++ schedule_api/information.py
@@ -1,7 +1,7 @@
 title = 'Splatoon 3 Schedule API'
 description = '''Unofficial Splatoon 3 schedule API
 '''
-version = 0.1.0
+version = '0.1.0'
 contact = {
   'name': 'SalmonRunKR',
   'url': 'https://twitter.com/SalmonRunKR',
