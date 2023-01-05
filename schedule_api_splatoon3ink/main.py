21a83220e2f417c40fe5178cd24a6a127d040887
munhyunsu <munhyunsu@gmail.com>
2023-01-06 06:48:20 +0900

    main for getter with splatoon3ink

---
+++ schedule_api_splatoon3ink/main.py
@@ -0,0 +1,22 @@
+def main():
+    if DEBUG:
+        print(f'[{time.time()-STIME}] Parsed arguements {FLAGS}')
+        print(f'[{time.time()-STIME}] Unparsed arguements {_}')
+
+
+if __name__ == '__main__':
+    import argparse
+
+    root_path = os.path.abspath(__file__)
+    root_dir = os.path.dirname(root_path)
+    os.chdir(root_dir)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', action='store_true',
+                        help='The present debug message')
+
+    FLAGS, _ = parser.parse_known_args()
+    DEBUG = FLAGS.debug
+
+    main()
+
