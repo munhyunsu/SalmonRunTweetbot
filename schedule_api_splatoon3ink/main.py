a0c6a46788b88bbf107c82cd08cda7691f9a1469
munhyunsu <munhyunsu@gmail.com>
2023-01-16 09:00:01 +0900

    DEBUG message

--- schedule_api_splatoon3ink/main.py
+++ schedule_api_splatoon3ink/main.py
@@ -102,9 +102,12 @@ def main():
     for coop_type in coop_types:
         for node in coop_schedules[coop_type]['nodes']:
             start_time = node['startTime'].replace('Z', '+00:00')
-            print(coop_type, node, 
-                  get_unixtime(start_time),
-                  get_time(get_unixtime(start_time), tz=TZ_SEOUL))
+            if DEBUG:
+                print(f'[{time.time()-STIME}] Read {coop_type} node')
+                print(node,
+                      get_unixtime(start_time),
+                      get_time(get_unixtime(start_time), tz=TZ_SEOUL))
+
         
         
 
